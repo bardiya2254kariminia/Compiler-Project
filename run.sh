24f002d4a44555f8d9ccf45b36421a3d540644a9
Bardiya2254kariminia <radiya2254@gmail.com>
2025-04-12 10:15:36 +0330

    fixed the buggs for building and running, first run biuld.sh and then run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cd build
 cd src
-./compiler "$(cat ../../input.txt)" > compiler.ll
-llc --filetype=obj -o=compiler.o compiler.ll
-clang -o compilerbin compiler.o ../../rtCompiler.c
-./compilerbin
+./compiler "$(cat ../../input.txt)" 
+#> compiler.ll
+#llc --filetype=obj -o=compiler.o compiler.ll
+#clang -o compilerbin compiler.o ../../rtCompiler.c
+#./compilerbin
